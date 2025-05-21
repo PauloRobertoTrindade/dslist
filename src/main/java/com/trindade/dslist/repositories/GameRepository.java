0fad8a816f4cbcc55c153896e189b187cbed5e43
Paulo Roberto Trindade <robertotrindade.filho@gmail.com>
2025-05-20 21:39:33 -0300

    Game repository, service, controller

---
+++ src/main/java/com/trindade/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.trindade.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trindade.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
