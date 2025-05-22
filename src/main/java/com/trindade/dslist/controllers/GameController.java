c8fce8524cef3393c76601c425855840389a9e79
Paulo Roberto Trindade <robertotrindade.filho@gmail.com>
2025-05-21 21:55:36 -0300

    GET game by id

--- src/main/java/com/trindade/dslist/controllers/GameController.java
+++ src/main/java/com/trindade/dslist/controllers/GameController.java
@@ -4,20 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trindade.dslist.dto.GameDTO;
 import com.trindade.dslist.dto.GameMinDTO;
-import com.trindade.dslist.entities.Game;
 import com.trindade.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
-	
+
 	@Autowired
 	private GameService gameService;
-	
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
